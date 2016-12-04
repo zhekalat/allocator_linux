b7b0277b1a7713129b0a54064dcac7121e36cbfe
Latyshev Evgeniy <e.latishev@gmail.com>
2016-12-04 21:37:06 +0400

    Refactoring

--- mylloc.c
+++ mylloc.c
@@ -4,10 +4,10 @@
 
 struct header 
 {
-    struct header *next, *last;
-    size_t        size;
-    int           is_available;
-    void         *memory;
+	struct header *next, *last;
+	size_t        size;
+	int           is_available;
+	void         *memory;
 };
 
 typedef struct header *Header;
@@ -17,20 +17,20 @@ Header start_point = NULL;
 
 Header find_memory(size_t size, Header *heap) 
 {
-    Header current = start_point;
+	Header current = start_point;
 	while (current && (!current->is_available || current->size < size))
 	{
 		*heap = current;
 		current = current->next;
 	}
-    return current;
+	return current;
 }
 
 void merge(Header current) 
 {
-    current->size = current->size + current->next->size + sizeof(struct header);
-    current->next = current->next->next;
-    if (current->next) 
+	current->size = current->size + current->next->size + sizeof(struct header);
+	current->next = current->next->next;
+	if (current->next) 
 	{
         current->next->last = current;
     }
@@ -50,65 +50,59 @@ void *malloc(size_t size)
         start_point->memory = NULL;
 		init = 1;
 	}
-    if (!size)
+	if (!size)
 		return NULL;
-    size_t length = MEM_ALIGN(size + sizeof(struct header));
-    Header last = NULL;
-    Header current = find_memory(size, &last);
-    if (!current) 
+	size_t length = MEM_ALIGN(size + sizeof(struct header));
+	Header last = NULL;
+	Header current = find_memory(size, &last);
+	if (!current) 
 	{
-        Header new_mem = sbrk(length);
-        if (new_mem == (void*) -1) 
-		{
-            return NULL;
-        }
-        new_mem->next = NULL;
-        new_mem->last = last;
-        new_mem->size = length - sizeof(struct header);
-        new_mem->memory = new_mem + 1;
-        last->next = new_mem;
-        current = new_mem;
-    }
-    current->is_available = 0;
-    return current->memory;
+        	Header new_mem = sbrk(length);
+        	if (new_mem == (void*) -1) 
+            		return NULL;
+        	new_mem->next = NULL;
+        	new_mem->last = last;
+        	new_mem->size = length - sizeof(struct header);
+        	new_mem->memory = new_mem + 1;
+        	last->next = new_mem;
+        	current = new_mem;
+	}
+	current->is_available = 0;
+	return current->memory;
 }
 
 void free(void *ptr) {
-    if (!ptr || ptr < (void *)start_point || ptr > sbrk(0))
+	if (!ptr || ptr < (void *)start_point || ptr > sbrk(0))
 		return;
-    Header current = (Header) ptr - 1;
-    if (current->memory != ptr)
+	Header current = (Header) ptr - 1;
+	if (current->memory != ptr)
 		return;
-    current->is_available = 1;
-    if (current->next && current->next->is_available) 
+	current->is_available = 1;
+	if (current->next && current->next->is_available) 
+		merge(current);
+	if (current->last->is_available) 
+		merge(current = current->last);
+	if (!current->next) 
 	{
-        merge(current);
-    }
-    if (current->last->is_available) 
-	{
-        merge(current = current->last);
-    }
-    if (!current->next) 
-	{
-        current->last->next = NULL;
-        sbrk(- current->size - sizeof(struct header));
-    }
+		current->last->next = NULL;
+		sbrk(- current->size - sizeof(struct header));
+	}
 }
 
 void *calloc(size_t nmemb, size_t size) 
 {
-    size_t length = nmemb * size;
-    void *ptr = malloc(length);
-    return ptr;
+	size_t length = nmemb * size;
+	void *ptr = malloc(length);
+	return ptr;
 }
 
 void *realloc(void *ptr, size_t size) 
 {
-    void *new_ptr = malloc(size);
-    if (new_ptr && ptr && ptr >= (void *)start_point && ptr <= sbrk(0)) 
+	void *new_ptr = malloc(size);
+	if (new_ptr && ptr && ptr >= (void *)start_point && ptr <= sbrk(0)) 
 	{
-        Header current = (Header) ptr - 1;
-        if (current->memory == ptr) 
+		Header current = (Header) ptr - 1;
+		if (current->memory == ptr) 
 		{
 			size_t length;
 			size_t i;
@@ -116,10 +110,10 @@ void *realloc(void *ptr, size_t size)
 				length = size;
 			else
 				length = current->size;
-            char *dst = new_ptr, *src = ptr;
-            for (i = 0; i < length; *dst = *src, ++src, ++dst, ++i);
-            free(ptr);
-        }
-    }
-    return new_ptr;
+			char *dst = new_ptr, *src = ptr;
+			for (i = 0; i < length; *dst = *src, ++src, ++dst, ++i);
+			free(ptr);
+		}
+	}
+	return new_ptr;
 }
